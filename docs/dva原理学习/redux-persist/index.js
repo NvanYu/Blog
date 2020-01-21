c5ed482261f394e837bced189286cb8717e70e8a
NvanYu <49123717+NvanYu@users.noreply.github.com>
2020-01-21 14:46:27 +0800

    Update index.js

--- docs/dva原理学习/redux-persist/index.js
+++ docs/dva原理学习/redux-persist/index.js
@@ -1,3 +1,7 @@
+/*
+* persistStore: 持久化仓库，对仓库的增强，给store加了一些方法，可以实现持久化
+* persistReducer: 增强我们的reducer的，添加了持久化
+*/
 import persistStore from "./persistStore";
 import persistReducer from "./persistReducer";
 
