a9e15eb0061c2db05c5d30f2c0d3446516429cb7
NvanYu <49123717+NvanYu@users.noreply.github.com>
2020-01-21 14:51:27 +0800

    Update react.js

--- docs/dva原理学习/redux-persist/integration/react.js
+++ docs/dva原理学习/redux-persist/integration/react.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 /**
-* PersistGate: 一个组件。渲染时可以让我们从存储引擎中获取数据并同步到仓库
+* PersistGate: 一个组件，渲染时可以让我们从存储引擎中获取数据并同步到仓库
 */
 class PersistGate extends React.Component {
   componentDidMount() {
